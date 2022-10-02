580243cfcd8ccd4dffb6b78bffce7caa8ec78344
Michelle Too <mjxtcodes@gmail.com>
2022-10-01 19:00:56 -0700

    adding some functions for rendering bookcards

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+//array and variable names are just placeholders for now, still need to figure out exactly how the API is formatted
+
 const url = 'https://openlibrary.org/dev/docs/api/books';
 
 fetch(url);
     .then((res) => res.json())
     .then(renderBookCards)
 
-function renderBookCards(//what to put for array here?//)
+function renderBookCards(bookcards) {
+    bookCard.forEach(renderBookCard)
+}
+function renderBookCard(bookcard) {
+    const bookCollectionDiv = document.getElementById('book-collection')
+    const bookCoverImage = document.createElement('img')
+
+    bookCoverImage.src = bookcards.image;
+    bookCollectionDiv.append(bookCoverImage)
+    bookCoverImage.addEventListener("click", e => renderDetails(bookcard))
+}   
+
+function el(elementName) {
+    return document.getElementById(elementName)
+}
+
+function renderDetails(bookcard) {
+    const bookName = el('book-title')
+    const bookGenre = el('genre')
+    const authorName = el('author-name')
+
+    bookName.textContent = bookcard.title;
+    bookGenre.textContent = bookcard.genre;
+    authorName.textContent = bookcard.author;
+}
