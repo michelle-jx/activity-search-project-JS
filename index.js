97d37fe90b11f306ac9e6049af771e31f262a3dd
Michelle Too <mjxtcodes@gmail.com>
2022-10-03 18:10:26 -0700

    basic html and JS

--- index.js
+++ index.js
@@ -1,254 +1,30 @@
-// Global Variable Declarations
-const mainDiv = document.getElementById('main-div')
-const search = document.getElementById('search')
-const previous = document.querySelector('#previous')
-const next = document.querySelector('#next')
-const mainCardType = document.getElementById('book-genre')
-const mainCardName = document.getElementById('book-title')
-const mainCardComments = document.getElementById('book-comments')
-const mainCardAddress = document.getElementById('book-author')
-const mainCardURL = document.getElementById('url')
-const mainCard = document.getElementById('Main-Book')
+const url = 'https://www.boredapi.com/api/';
 
-let pageNumber = 1
-let stateSearchParameter = ""
-let citySearchParameter = ""
-let zipcodeSearchParameter = ""
-let parameter = ""
+fetch(url)
+.then((res) => res.json())
+.then(renderMysteryCards)
 
-// Function to Clear Page and Reload Original Books
-function clearPage(){
-    pageReset()
-    fetch (`https://openlibrary.org/books`)
-    .then(res => res.json())
-    .then(data => {
-        data.forEach(bookBuilder)
-        hidePrevious()
-        hideButtonIfNextPage()
-    })
-    parameter = ""
-    pageNumber = 1
-}
-clearPage()
-
-// Function to Delete All Items in Main Div (Brewery Cards)
-function pageReset(){
-    while (mainDiv.firstChild) {
-        mainDiv.removeChild(mainDiv.lastChild);
-    }
-}
-
-// Hide Previous Button
-function hidePrevious(){
-    previous.classList.add('hidden')
-}
-// Show Previous Button
-function showPrevious(){
-    previous.classList.remove('hidden')
-}
-// Hide Next Button
-function hideNext(){
-    next.classList.add('hidden')
-}
-// Show Next Button
-function showNext(){
-    next.classList.remove('hidden')
-}
-
-function hideButtonIfNextPage(){
-    fetch(`https://api.openbrewerydb.org/breweries?${parameter}page=${pageNumber + 1}&per_page=12`)
-    .then(res => res.json())
-    .then(data => {
-        if(data.length === 0){
-            hideNext()
-        }
-        else{
-            showNext()
-        }
+function renderMysteryCards(mysteryCards) {
+    mysteryCards.forEach(mysteryCard => {
+        const mysteryCard = makeMysteryCard(mysteryCard)
     })
 }
 
-// Search Form Event Listener
-search.addEventListener('submit', (e) => {
-    (e).preventDefault()
-    pageReset()
-    pageNumber = 1
-
-    let author = e.target.author.value
-    let title = e.target.title.value
-    let genre = e.target.genre.value
-
-    authorSearchParameter = `by_state=${state}`
-    titleSearchParameter = `by_city=${city}`
-    genreSearchParameter = `by_postal=${zipcode}`
-
-    parameter = `${authorSearchParameter}&${titleSearchParameter}&${genreSearchParameter}&`
-
-    fetch(`https://api.openbrewerydb.org/breweries?${parameter}page=1&per_page=12`)
-    .then(res => res.json())
-    .then(data => data.forEach(bookBuilder))
-
-    hideButtonIfNextPage()
-    hidePrevious()
-})
-
-// Reset Button
-const reset = document.querySelector('#reset')
-reset.addEventListener('click', function(){
-    search.reset()
-    clearPage()
-})
-
-// Event Listener for Next Button
-next.addEventListener('click', function(){
-    pageNumber += 1
-    fetch (`https://api.openbrewerydb.org/breweries?${parameter}page=${pageNumber}&per_page=12`)
-    .then(res => res.json())
-    .then(data => {
-        hideButtonIfNextPage()
-        if(data.length === 0){
-            pageNumber -= 1
-        }
-        else{
-            pageReset()
-            data.forEach(bookBuilder)
-        }
-
-    if(pageNumber > 1){
-        showPrevious()
-    }      
-})
-})
-
-// Event Listener for Previous Button
-previous.addEventListener('click', function(){
-    if(pageNumber > 1){
-        pageReset()
-        pageNumber -= 1
-        fetch (`https://api.openbrewerydb.org/breweries?${parameter}page=${pageNumber}&per_page=12`)
-        .then(res => res.json())
-        .then(data => data.forEach(bookBuilder))
-        hideButtonIfNextPage()
-        if (pageNumber === 1){
-            hidePrevious()
-        }
-    }
-})
-
-const commentForm = document.querySelector('.comment-form')
-commentForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const comment = e.target.comments.value
-    const commentP = document.createElement('p')
-    commentP.textContent = `○ ${comment}`
-    mainCardComments.appendChild(commentP)
-    commentForm.reset()
-})
-
-// Builds the Brewery Cards from Incoming Data
-function bookBuilder(data) {
-    const breweryContainer = document.createElement('div')
-    breweryContainer.className = 'beer-card'
-
-    const breweryName = document.createElement('h2')
-    breweryName.textContent = data.name
-
-    let phoneNumber = "Not Available"
-    if(data.phone){
-        const phone = (data.phone).toString()
-        phoneNumber = `${phone.substr(0,3)}-${phone.substr(3,3)}-${phone.substr(6,4)}`
-    }
-
-    let postalCode = "Not Available"
-    if(data.postal_code !== 0){
-        postalCode = (data.postal_code).toString().substr(0,5)
-    }
-
-    const breweryAddress = document.createElement('p')
-    const shortAddress = `${data.city}, ${data.state}, U.S.`
-    const longAddress = `${data.city}, ${data.state}, U.S. ${postalCode} (${phoneNumber})`
-    breweryAddress.textContent = `${shortAddress}`
-
-    // const breweryRater = document.createElement('form')
-    // const starRating = document.createElement('div')
-    // breweryRater.id = 'Rating'
-    // starRating.id = 'star-form'
-
-    //breweryRater
-    breweryContainer.append(breweryName, breweryAddress, starCreator())
-    // breweryRater.append(starRating)
-    // for (element of starBuilder()) {
-    //     starRating.append(element)
-    // }
-
-
-    // Event Listener to Display Brewery onto Main Card
-    breweryContainer.addEventListener('click', () => {
-        mainCardName.textContent = data.name
-        let street = `${data.street}, `
-        if(data.street === null){
-            street = ""
-        }
-        mainCardAddress.textContent = `${street}${longAddress}`
-        mainCardType.textContent = `Brewery Type: ${data.brewery_type}`
-        mainCardURL.textContent = ""
-        if(data.website_url){
-            mainCardURL.textContent = `${data.website_url}`
-            mainCardURL.href = data.website_url
-        }
-
-        mainCardComments.textContent = `Comments: `
+//this is for ID dropdown menu??
+const listenForFormSubmit() {
+    const form = document.querySelector('.category-identifier')
+    form.addEventListener('submit', e => {
+        e.preventDefault();
     })
-    mainDiv.append(breweryContainer)
 }
 
-// function starBuilder () {
-//     const starArray = []
-//     for (i=1; i < 6; i++) {
-//         const star = document.createElement('input')
-//         star.type = 'radio'
-//         star.name = `star${i}`
-//         star.rating = `star-rating${i}`
-//         star.value = `star-value${i}`
-//         starArray.push(star)
-//     }
-//     return(starArray)
-// }
-
-
-// Different attempt of creating a star
-function starCreator(){
-    const starContainer = document.createElement('div')
-    starContainer.classList.add('starContainer')
-    for(i = 1; i < 6; i++){
-        const star = document.createElement('a')
-        star.classList.add('star')
-        star.textContent = '⭐'
-        starContainer.appendChild(star)
-    }
-    const starArray = Array.from(starContainer.children)
-    starArray.forEach((element, indx) => {
-        element.addEventListener('click', () => {
-            starArray.forEach((innerElement, innerIndex) => {
-                if(indx >= innerIndex){
-                    innerElement.classList.add('active')
-                }
-            })
-        starContainer.classList.add('disable')
-        // fetch('http://localhost:3000/brewery', {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //         "Accept": "application/json",
-        //     },
-        //     body: JSON.stringify({
-        //         "rating": indx + 1      
-        //     }) 
-        // })
-        })
-    })
-    return starContainer
+const makeMysteryCard(mysteryCard) {
+    const mysteryCard = document.createElement()
+    const activityName = document.createElement()
+    const accessibilityNum = document.createElement()
+    const activityType = document.createElement()
+    const participantNum = document.createElement()
+    const activityPrice = docuemnt.createElement()
+    const activityLink = document.createElement()
+    const activityKey = document.createElement()
 }
-const mainContainer = document.querySelector('.main-Container')
-mainContainer.append(starCreator())
-
